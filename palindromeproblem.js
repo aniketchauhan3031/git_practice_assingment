8e45bacb0075fb71b2c7d73ad8d9bc6089cb7c86
aniketchauhan3031 <115460296+aniketchauhan3031@users.noreply.github.com>
2022-11-06 20:14:08 +0530

    prime and palindrome files created

---
+++ palindromeproblem.js
@@ -0,0 +1,8 @@
+let len=string.length;
+for(let i=0;i<len/2;i++){
+if(string[i]==srting[len-1-1]){
+console.log("number is palindrome");
+else
+console.log("number is not palindrome");
+}
+}
---
+++ primeproblem.js
@@ -0,0 +1,9 @@
+let num= 29
+for(let  i=2;i<=num/2;++i){
+if(num%i!=0){
+console.log("number is prime"){
+else
+consloe.log("number is not prime")
+}
+}
+}
